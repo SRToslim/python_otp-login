b1c557aa47039a28bddfa86f895da536b210103a
Rubaiyat Toslim <srtoslim@gmail.com>
2023-10-09 21:58:09 +0600

    urls.py

--- otplogin/urls.py
+++ otplogin/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('mobile/', mobile_login, name='mobile'),
     path('mobile/otp/', verify_otp, name='otp'),
     path('mobile/forget-password/', password_otp, name='forget-password'),
+    path('mobile/forget-password/otp', reset_password_otp_verify, name='forget-password-otp'),
 ]
